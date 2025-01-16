481d6faafa4dd498302d2d7e94d04bef33d9edbd
niklaseberl <eberl.nik@gmail.com>
2025-01-16 16:45:18 +0100

    Implement First Tests

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,32 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
-    {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        var app = new GildedRose(items);
-        app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
-    }
+            public void NormalItem_DegradesQualityByOne_BeforeSellIn()
+            {
+                // Arrange
+                var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 10, Quality = 20 } };
+                var app = new GildedRose(items);
+
+                // Act
+                app.UpdateQuality();
+
+                // Assert
+                Assert.AreEqual(9, items[0].SellIn);
+                Assert.AreEqual(19, items[0].Quality);
+            }
+
+    [Test]
+            public void AgedBrie_IncreasesQualityOverTime()
+            {
+                // Arrange
+                var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+                var app = new GildedRose(items);
+
+                // Act
+                app.UpdateQuality();
+
+                // Assert
+                Assert.AreEqual(1, items[0].SellIn);
+                Assert.AreEqual(1, items[0].Quality);
+            }
 }
